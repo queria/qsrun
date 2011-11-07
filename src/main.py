65593c8a7e2ec1cbb163a0ffb1b1532777b2a5c7
Queria Sa-Tas <queria@sa-tas.net>
2011-11-07 03:58:02 +0100

    removed mlockall (added in c364ceb2) - no obvious benefit spotted

--- src/main.py
+++ src/main.py
@@ -4,20 +4,12 @@ from PySide.QtGui import QApplication
 import platform
 import gui
 from singleapp import SingleApp, OtherInstanceFoundError
-from ctypes import cdll, c_int
 
-# we prepare MCL_FUTURE constant
-# see mlock(2) and /usr/include/asm-generic/mman.h
-# for your value
-C_MCL_CURRENT = c_int(1)
-C_MCL_FUTURE = c_int(2)
 
 def main():
     if platform.system() != 'Linux':
         print 'Only linux system is supported at the moment. Sorry!'
         sys.exit(2)
-
-    libc = cdll.LoadLibrary('libc.so.6')
 
     app = QApplication(sys.argv)
 
@@ -32,9 +24,7 @@ def main():
 
     runbar.show()
 
-    libc.mlockall(C_MCL_CURRENT) # always stay in RAM
     result = app.exec_()
-    libc.munlockall();
     sys.exit(result)
 
 if __name__ == '__main__':
